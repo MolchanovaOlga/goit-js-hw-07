8cffec89176fd6192cbacb6b588edcfeab3cf06f
MolchanovaOlga <designer.molchanova@gmail.com>
2023-11-29 23:41:51 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
 const ulCategories = document.querySelectorAll('.item');
 console.log(`Number of categories: ${ulCategories.length}`);
 
-for (const category of ulCategories) {
+ulCategories.forEach(category => {
     console.log(`Category: ${category.firstElementChild.textContent}`);
     
     const items = category.querySelectorAll('ul>li');
     console.log(`Elements: ${items.length}`);
-}
+})
